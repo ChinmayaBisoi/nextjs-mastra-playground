a82995c74273a755ab0cc82866c26f81a45e02cf
ChinmayaBisoi <bisoi.chinmaya.1999@gmail.com>
2025-11-19 17:38:58 +0530

    update: landing page

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@mastra/*"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "html.tailus.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
